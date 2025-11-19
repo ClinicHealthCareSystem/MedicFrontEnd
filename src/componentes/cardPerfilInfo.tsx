6f15d7b71aa2dd12c61f79b8811e1f08210a04b2
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-19 16:19:06 -0300

    TSK-12 excluir alagumas informações do card perfil

--- src/componentes/cardPerfilInfo.tsx
+++ src/componentes/cardPerfilInfo.tsx
@@ -7,29 +7,17 @@ import { useTheme } from "../hooks/ThemeContext";
 export default function CardPerfilInfo() {
   const { colors } = useTheme();
   const cardPerfilInfoStyles = getCardPerfilInfoStyles(colors);
-  const [isEditing, setIsEditing] = useState(false);
+  
 
   return (
     <View style={cardPerfilInfoStyles.cardInfo}>
       <View style={cardPerfilInfoStyles.cardInfoTittle}>
         <Text style={cardPerfilInfoStyles.tittleInfo}>Informações Pessoais</Text>
-        <TouchableOpacity
-          style={cardPerfilInfoStyles.editButton}
-          onPress={() => setIsEditing(!isEditing)}
-        >
-          <MaterialCommunityIcons
-            name={isEditing ? "check" : "account-edit"}
-            size={30}
-            color={"white"}
-          />
-        </TouchableOpacity>
       </View>
-
       <Text style={cardPerfilInfoStyles.labelInfo}>Nome:</Text>
       <View style={cardPerfilInfoStyles.textInfo}>
         <TextInput
           style={cardPerfilInfoStyles.inputInfo}
-          editable={isEditing}
           placeholder="José"
         />
       </View>
@@ -38,35 +26,7 @@ export default function CardPerfilInfo() {
       <View style={cardPerfilInfoStyles.textInfo}>
         <TextInput
           style={cardPerfilInfoStyles.inputInfo}
-          editable={isEditing}
           placeholder="Jose@gmail.com"
-        />
-      </View>
-
-      <Text style={cardPerfilInfoStyles.labelInfo}>Telefone:</Text>
-      <View style={cardPerfilInfoStyles.textInfo}>
-        <TextInput
-          style={cardPerfilInfoStyles.inputInfo}
-          editable={isEditing}
-          placeholder="(85) 9 9999-9999"
-        />
-      </View>
-
-      <Text style={cardPerfilInfoStyles.labelInfo}>Data de Nascimento:</Text>
-      <View style={cardPerfilInfoStyles.textInfo}>
-        <TextInput
-          style={cardPerfilInfoStyles.inputInfo}
-          editable={isEditing}
-          placeholder="01/01/2001"
-        />
-      </View>
-
-      <Text style={cardPerfilInfoStyles.labelInfo}>Endereço:</Text>
-      <View style={cardPerfilInfoStyles.textInfo}>
-        <TextInput
-          style={cardPerfilInfoStyles.inputInfo}
-          editable={isEditing}
-          placeholder="Rua A 10"
         />
       </View>
     </View>
